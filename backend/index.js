bb23c509ecc913778d14ff7cabb8c183f229a689
OzanYildirimPXL <ozan.yildirim@student.pxl.be>
2024-05-09 13:05:44 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -25,28 +25,6 @@ app.use(
     })
 );
 
-// Hash het wachtwoord voordat je het in de database invoegt
-const hashPassword = (password) => {
-    return pool.query('SELECT crypt($1, gen_salt(\'bf\')) AS hash', [password])
-        .then(result => result.rows[0].hash);
-};
-
-app.post('/register', async (request, response) => {
-    const { username, password } = request.body;
-
-    const hashedPassword = await hashPassword(password);
-
-    const query = 'INSERT INTO users (user_name, password) VALUES ($1, $2)';
-    const values = [username, hashedPassword];
-
-    pool.query(query, values, (error, result) => {
-        if (error) {
-            throw error;
-        }
-        response.status(201).send(`User ${username} added with hashed password.`);
-    });
-});
-
 app.post('/authenticate', async (request, response) => {
     const { username, password } = request.body;
 
